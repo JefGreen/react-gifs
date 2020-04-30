cab66174c536aa7a59ed8918579c3ca48e52b777
Jeff Trempe <trempe.jeff@gmail.com>
2020-04-29 22:48:13 -0400

    Added gifs list

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,9 +2,16 @@ import React, { Component } from 'react';
 
 import SearchBar from './search_bar.jsx';
 import Gif from './gif.jsx';
+import GifList from './gif_list.jsx';
+
 
 class App extends Component {
   render() {
+    const gifs = [
+      {id: "fcIeSjxlIhWEg"},
+      {id: "4Nmo4QxNEeeKaK8pUk"}
+    ];
+
     return (
       <div>
         <div className="left-scene">
@@ -13,7 +20,9 @@ class App extends Component {
             <Gif id="drON1hl3kv9hm"/>
           </div>
         </div>
-        <div className="right-scene"></div>
+        <div className="right-scene">
+          <GifList gifs={gifs} />
+        </div>
       </div>
     );
   }
